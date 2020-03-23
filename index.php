10aa56df6b7cb289b2da1001fa0729a09c2e719b
Maxim Leyzerovich <contact@round.is>
2020-03-22 22:07:11 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(0);
+$page_content = file_get_contents($_GET['url']);
+$dom_obj = new DOMDocument();
+$dom_obj->loadHTML($page_content);
+$asset_url = null;
+foreach($dom_obj->getElementsByTagName('meta') as $meta) {
+  if($meta->getAttribute('property')=='og:image'){
+    $asset_url = $meta->getAttribute('content');
+  }
+}
+
+if (isset($_GET['echo'])) {
+  echo $asset_url;
+} else {
+  header("Location: " . $asset_url);
+}
+
+?>
